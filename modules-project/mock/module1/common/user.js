c79200a9615558db395456e5456b17db89137f2b
张少龙 <zhangshaolong@baijiahulian.com>
2015-11-16 17:14:34 +0800

    删除demo

--- modules-project/mock/module1/common/user.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "status" : 200,
-    "data": {
-        id: 1,
-        name: "张三"
-    }
-};
--- modules-project/src/base.js
+++
@@ -1,6 +0,0 @@
-define(function (require, exports) {
-
-    // 启动模块初始化
-    require('common/moduleHanlder').init();
-
-});
